9acc003c3cfd4c75227b63a0a11346a1559c7606
Stanley Muzhuthettu <smuzh001@ucr.edu>
2019-12-08 16:56:55 -0800

    added N fold cross validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-import numpy
+import numpy as np
+import math
 
 
 def leave_one_out_cross_validation(x, curr_set):
@@ -17,33 +18,56 @@ def leave_one_out_cross_validation(x, curr_set):
     }
     return switch.get(x, 0)
 
+def leave_one_out(data, k):
+    num_correct = 0
+     #iterate through rows
+    for i in range(0, len(data) - 1):
+        #print('I am looping over the rows '+ str(i) )
+        #check feature of the rows
+        best_so_far = sys.float_info.max
+        best_loc = None
+        accuracy = None
+        for j in range(0, len(data)- 1):
+            if i != j:
+                #print('for exemplar '+str(i)+' i am comparing to '+ str(j))
+                distance = math.sqrt((data[i][k] - data[j][k])**2 )
+                if distance < best_so_far:
+                    best_so_far = distance
+                    best_loc = j
+
+        print('for exemplar '+ str(i)+ ' I believe its NN is '+str(best_loc))
+        if data[i][0] == data[best_loc][0]:
+            num_correct += 1
+            #print('exempler '+str(i)+' is correct')
+    Accuracy = num_correct / len(data)
+    #print(Accuracy)
+    return Accuracy
+
 def ForwardSelection():
-    print('Foward Selection is cool')
+    #print('Foward Selection is cool')
     with open('CS170_SMALLtestdata__27.txt') as file:
         result = [[float(val) for val in line.split()] for line in file]
-    #print(result[0])
+    print(result[0][1])
     featureCount = len(result[0]) - 1
     curr_set = set()
-
-    for i in range(1, featureCount + 1):
-        feature_to_add_at_this_level = []
-        best_acc_so_far = 0
+    print(leave_one_out(result, 1))
+    # for i in range(1, featureCount + 1):
+    #     feature_to_add_at_this_level = []
+    #     best_acc_so_far = 0
         
-        for j in range(1, featureCount + 1):
-            #ignore if j is already part of set
-            if j in curr_set:
-                continue
-            print('\tConsidering adding the '+ str(j)+ ' feature')
-            accuracy = leave_one_out_cross_validation(j,curr_set)
-            if accuracy > best_acc_so_far:
-                best_acc_so_far = accuracy
-                feature_to_add_at_this_level = j        
+    #     for j in range(1, featureCount + 1):
+    #         #ignore if j is already part of set
+    #         if j in curr_set:
+    #             continue
+    #         #print('\tConsidering adding the '+ str(j)+ ' feature')
+    #         accuracy = leave_one_out_cross_validation(j,curr_set)
+    #         if accuracy > best_acc_so_far:
+    #             best_acc_so_far = accuracy
+    #             feature_to_add_at_this_level = j        
         
-        print('best accuracy this level: ' + str(feature_to_add_at_this_level) +' with an accuracy of '+str(best_acc_so_far))
-        curr_set.add(feature_to_add_at_this_level)        
-        print('On the '+ str(i) + ' level of the search tree')
-
-
+    #     print('best accuracy this level: ' + str(feature_to_add_at_this_level) +' with an accuracy of '+str(best_acc_so_far))
+    #     curr_set.add(feature_to_add_at_this_level)        
+    #     #print('On the '+ str(i) + ' level of the search tree')
 
 
 
