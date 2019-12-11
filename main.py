b27e8e1c7d8f1944d1f79205ff7099b55dfa374a
smuzh001@ucr.edu <smuzh001@ucr.edu>
2019-12-11 14:55:05 -0800

    finished Originial Algorithm (Foward Selectiion w Pruning also refactored code

--- main.py
+++ main.py
@@ -2,21 +2,6 @@ import sys
 import math
 
 
-def leave_one_out_cross_validation(x, curr_set):
-    switch = {
-        1: 0.84,
-        2: 0.87,
-        3: 0.79,
-        4: 0.88,
-        5: 0.64,
-        6: 0.45,
-        7: 0.87,
-        8: 0.86,
-        9: 0.65,
-        10: 0.12,
-    }
-    return switch.get(x, 0)
-
 #data = our current data file, curr_set = our current set of features (our testing set), k = our new feature we are considering
 def leave_one_out(data, curr_set, k):
     num_correct = 0
@@ -39,20 +24,21 @@ def leave_one_out(data, curr_set, k):
                     best_loc = j
 
         #print('for exemplar '+ str(i)+ ' I believe its NN is '+str(best_loc))
+        #check if NN is same class
         if data[i][0] == data[best_loc][0]:
             num_correct += 1
             #print('exempler '+str(i)+' is correct')
     Accuracy = num_correct / len(data)
-    #print(Accuracy)
+    #print(num_correct)
     return Accuracy
 
 
-def ForwardSelection():
+def ForwardSelection(path):
     #print('Foward Selection is cool')
     best_set = set()
     best_acc = 0
 
-    with open('CS170_SMALLtestdata__27.txt') as file:
+    with open(path) as file:
         result = [[float(val) for val in line.split()] for line in file]
     
     featureCount = len(result[0]) - 1
@@ -81,6 +67,9 @@ def ForwardSelection():
             best_set = curr_set.copy()   
         #print('On the '+ str(i) + ' level of the search tree')
     print('On small dataset, the error rate can be '+str(best_acc) + ' when using only features: '+ str(best_set))
+
+
+
 
 
 def BackwardHelper(data, curr_set, k):
@@ -114,9 +103,9 @@ def BackwardHelper(data, curr_set, k):
     return Accuracy
 
 
-def BackwardElimination():
+def BackwardElimination(path):
     #print('Backward Elimination is cool')
-    with open('CS170_SMALLtestdata__27.txt') as file:
+    with open(path) as file:
         result = [[float(val) for val in line.split()] for line in file]
     featureCount = len(result[0])
     curr_set = set()
@@ -153,24 +142,108 @@ def BackwardElimination():
 
     print('On small dataset, the error rate can be '+str(best_acc) + ' when using only features: '+ str(best_set))
 
-    
 
-def OGAlgorithm():
-    print('My own original algorithm')
+def leave_one_out_prune(data, curr_set, k, prune_limit):
+    num_correct = 0
+    curr_miss_count = 0
+
+    for i in range(0, len(data) - 1): #validate current row
+        best_so_far = sys.float_info.max
+        best_loc = None
+        for j in range(0, len(data)- 1):    #figure out which is the NN
+            if i != j:
+                distance = 0
+                for feature in curr_set:
+                    distance += (data[i][feature] - data[j][feature] )**2
+                distance = math.sqrt(distance + (data[i][k] - data[j][k] )**2 )
+
+                if distance < best_so_far:
+                    best_so_far = distance
+                    best_loc = j
+
+
+        if data[i][0] == data[best_loc][0]:
+            num_correct += 1
+        else:
+            curr_miss_count += 1
+            if curr_miss_count > prune_limit:
+                return -1
+        
+        #check if curr_num_correct 
+
+    return num_correct
+
+
+def OGAlgorithm(path):
+    #print('Foward Selection is cool')
+    best_set = set()
+    best_acc = 0
+
+    with open(path) as file:
+        result = [[float(val) for val in line.split()] for line in file]
+    
+    featureCount = len(result[0])
+    curr_set = set()
+    
+    for i in range(1, featureCount):
+        feature_to_add_at_this_level = None
+        best_acc_so_far = 0
+        min_miss_count = len(result)
+
+        for j in range(1, featureCount):
+            #ignore if j is already part of set
+            if j in curr_set:
+                continue
+            #print('\tConsidering adding the '+ str(j)+ ' feature')
+            correct_count = leave_one_out_prune(result, curr_set ,j, min_miss_count)
+            
+            #fell under prune limit
+            if correct_count == -1:
+                continue
+            
+            accuracy = correct_count / len(result)
+
+            #update our accuracy
+            if accuracy > best_acc_so_far:
+                best_acc_so_far = accuracy
+                min_miss_count = len(result) - correct_count
+                feature_to_add_at_this_level = j
+
+
+        print('most accurate feature at level '+str(i)+': ' + str(feature_to_add_at_this_level) +' with an accuracy of '+str(best_acc_so_far))
+        
+        curr_set.add(feature_to_add_at_this_level)
+
+        if best_acc_so_far > best_acc:
+            best_acc = best_acc_so_far
+            best_set = curr_set.copy()   
+        #print('On the '+ str(i) + ' level of the search tree')
+    print('On small dataset, the error rate can be '+str(best_acc) + ' when using only features: '+ str(best_set))
 
 
 while True:
     #print(dataSet.read())
-    print('Welcome to Stanley Muzhuthettu Feature Generation Extravaganza\nPlease select the wrapper:')
+    print('Welcome to Stanley Muzhuthettu Feature Generation Extravaganza\nPlease select the dataset 1: small 27. Or 2: large 29')
+    choice = int(sys.stdin.readline())
+
+    if choice == 1:
+        path = 'CS170_SMALLtestdata__27.txt'
+    elif choice == 2:
+        path = 'CS170_LARGEtestdata__29.txt'
+    else:
+        print('Invalid Entry')
+        continue
+
     print('Forward Selection: 1\nBackward Elimination: 2\nYour original search algorithm: 3\n')
     choice = int(sys.stdin.readline())
 
     if choice == 1:
-        ForwardSelection()
+        ForwardSelection(path)
     elif choice == 2:
-        BackwardElimination()
+        BackwardElimination(path)
     elif choice == 3:
-        OGAlgorithm()
+        OGAlgorithm(path)
     else:
         print('Invalid Entry')
+        continue
 
